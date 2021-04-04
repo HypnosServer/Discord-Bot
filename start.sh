7c7c371ff3781ba1d1af2892e0cbce33d63b8fcc
NotCreative <61510083+NotCreative21@users.noreply.github.com>
2021-04-04 07:34:42 -0400

    woops

--- start.sh
+++ start.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 while true
 echo "Bot Script started | autorestart on"
-do node index.js
+do 
+	node index.js
 	echo "Restarting in 5 seconds, ctrl^c to cancel."
 	sleep 5
 done
